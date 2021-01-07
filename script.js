f02d9328023dd9f97818474028ff033db0e02c01
harlenback <harlenback@gmail.com>
2021-01-07 11:53:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+let apiKey = `39c117dd`;
+let searchField = document.getElementById(`searchInput`);
+let results = document.getElementById(`results`);
+let p = document.querySelector(`p`);
+let list = document.querySelector(`ul`);
+let movieDiv = document.getElementById(`movieDiv`);
+
+function search(searchType, searchWord) {
+  fetch(`http://www.omdbapi.com/?${searchType}=${searchWord}&apikey=${apiKey}`)
+    .then((response) => response.json())
+    .then((movieData) => {
+      if (searchType === `s`) {
+        movieList(movieData);
+      } else if (searchType === `i`) {
+        showUniqueMovie(movieData);
+      } else {
+        console.log(`Something went wrong`);
+      }
+    });
+}
+
+function choseUniqueMovie(movieData) {
+  search(`i`, movieData);
+}
+
+function showUniqueMovie(movieData) {
+  // Orkar jag förbättra denna?
+  let dataTest = JSON.stringify(movieData);
+
+  movieDiv.innerHTML = `
+     <img src="${movieData.Poster}" alt="" srcset="">
+     <br>
+     <br>
+     ${dataTest}
+  `;
+}
+
+searchField.addEventListener(`keyup`, function () {
+  movieDiv.innerHTML = ``
+  if (searchField.value.length > 2) {
+    let searchWord = searchField.value;
+    search(`s`, searchWord);
+  }
+});
+
+const movieList = (movieData) => {
+  console.log(movieData);
+  if (movieData.Error === "Movie not found!") {
+    p.innerHTML = ``;
+    list.innerHTML = ``;
+
+    results.style.backgroundColor = `pink`;
+    p.textContent = `Inga sökträffar`;
+  } else {
+    results.style.backgroundColor = `green`;
+    p.innerHTML = ``;
+    list.innerHTML = ``;
+
+    for (let i = 0; i < movieData.Search.length; i++) {
+      console.log(movieData.Search[i]);
+
+      const newLi = document.createElement(`li`);
+      newLi.textContent = `${movieData.Search[i].Title} (${movieData.Search[i].Year})`;
+      newLi.setAttribute(
+        "onclick",
+        `choseUniqueMovie("${movieData.Search[i].imdbID}")`
+      );
+      list.appendChild(newLi);
+      console.log(newLi);
+    }
+  }
+};
